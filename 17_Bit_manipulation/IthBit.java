2f4e78b3081e8aea04ed2c62bf7cab5cbb7813e7
PrathmeshBodake <prathmeshbodake007@gmail.com>
2025-11-27 18:07:03 +0530

    added clear ith

--- 17_Bit_manipulation/IthBit.java
+++ 17_Bit_manipulation/IthBit.java
@@ -14,6 +14,11 @@ public class IthBit {
     public static int set_ith(int x , int i){
         return (x | (1 << i));
     }
+    public static int clear_ith(int x , int i){
+        int bitMash = 1 << i;
+        bitMash = ~(bitMash);
+        return (x & bitMash);
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
@@ -23,6 +28,7 @@ public class IthBit {
 
         System.out.println(ith_bit(x ,i));
         System.out.println(set_ith(x ,i));
+        System.out.println(clear_ith(x ,i));
 
     }
 }
