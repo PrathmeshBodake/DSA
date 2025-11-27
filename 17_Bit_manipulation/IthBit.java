69386658b4c37f15474d3e7634eb46f99b6772ff
PrathmeshBodake <prathmeshbodake007@gmail.com>
2025-11-27 17:47:54 +0530

    get ith value or ith position , set ith or convert if  0 -> 1

---
+++ 17_Bit_manipulation/IthBit.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class IthBit {
+    
+    public static int ith_bit(int x , int i){
+        int bitMask = 1 << i;
+        if((x & bitMask)==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    public static int set_ith(int x , int i){
+        return (x | (1 << i));
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int x = sc.nextInt();
+        System.out.print("Enter a Bit number that you want: ");
+        int i = sc.nextInt();
+
+        System.out.println(ith_bit(x ,i));
+        System.out.println(set_ith(x ,i));
+
+    }
+}
