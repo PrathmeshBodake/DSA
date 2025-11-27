f5f0bdd0791bb2968075bd05053be986b3df0ea2
PrathmeshBodake <prathmeshbodake007@gmail.com>
2025-11-27 12:04:02 +0530

    Basic bitwise &

---
+++ 17_Bit_manipulation/BitwiseAnd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BitwiseAnd{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number a : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter a number b : ");
+        int b = sc.nextInt();
+
+        int result = a & b;
+        System.out.println("a & b = "+ result);
+
+    }
+}
