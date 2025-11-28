1df1dc5388fee7adc7a3971494d06bc051851501
PrathmeshBodake <prathmeshbodake007@gmail.com>
2025-11-29 00:10:19 +0530

    write to how it works

--- 17_Bit_manipulation/IsItPowOf2.java
+++ 17_Bit_manipulation/IsItPowOf2.java
@@ -4,6 +4,10 @@ public class IsItPowOf2 {
     
     public static boolean power(int n){
         return (n&(n-1))==0;
+        // ex 15        16 
+        // 1111         10000
+        // 1110         01111
+        // 1110         00000
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
