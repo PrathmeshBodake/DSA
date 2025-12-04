ab8e24bf881088685024786c06d4ae3226d27be1
PrathmeshBodake <prathmeshbodake007@gmail.com>
2025-12-04 23:34:11 +0530

    Binary String with not consectives of ones

---
+++ 19_Recursion_basics/BinString.java
@@ -0,0 +1,25 @@
+public class BinString {
+
+    public static void binary(int n , int lastplace , String str){
+
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        // if(lastplace ==0 ){
+
+        //     binary(n-1, 0, str+="0");
+        //     binary(n-1, 1, str+="1");
+        // }else{
+        //     binary(n-1, 0, str+="0");
+           
+        // }
+        binary(n-1, 0, str+"0");
+        if(lastplace == 0){
+            binary(n-1, 1, str+"1");
+        }
+    }
+    public static void main(String args[]){
+        binary(3, 0, "");
+    }
+}
